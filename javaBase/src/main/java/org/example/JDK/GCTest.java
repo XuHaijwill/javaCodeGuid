333581aba4df1dce695aaabe36ed8159db903f0c
XuHaijwill <zuiwoxuanlou@163.com>
2022-12-11 15:03:29 +0800

    code commit

--- javaBase/src/main/java/org/example/JDK/GCTest.java
+++ javaBase/src/main/java/org/example/JDK/GCTest.java
@@ -1,4 +1,15 @@
 package org.example.JDK;
 
 public class GCTest {
+    public static void main(String[] args) throws InterruptedException {
+        /*allocation1, allocation2/*, allocation3, allocation4, allocation5, allocation6*/
+        byte[] allocation1 = new byte[60000 * 1024];
+
+        //allocation2 = new byte[8000*1024];
+
+         /*allocation3 = new byte[1000*1024];
+ allocation4 = new byte[1000*1024];
+ allocation5 = new byte[1000*1024];
+ allocation6 = new byte[1000*1024];*/
+    }
 }
