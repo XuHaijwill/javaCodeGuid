86b745e5e70d85dae2c03fe677a2da2a5e32f234
XuHaijwill <zuiwoxuanlou@163.com>
2023-03-11 21:03:17 +0800

    rmi chat code

---
+++ javaBase/src/main/java/org/example/rmiChat/client/ChatClient3IF.java
@@ -0,0 +1,19 @@
+package org.example.rmiChat.client;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * Remote interface for client classes
+ * A method to receive a string
+ * A method to update changes to user list
+ *
+ *
+ */
+public interface ChatClient3IF extends Remote{
+
+	public void messageFromServer(String message) throws RemoteException;
+
+	public void updateUserList(String[] currentUsers) throws RemoteException;
+	
+}
